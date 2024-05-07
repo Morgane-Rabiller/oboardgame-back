b6effccaf3595646c30a366ad95fab29a13f4c22
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-05-07 14:01:41 +0200

    added associations model

---
+++ models/associations.js
@@ -0,0 +1,19 @@
+const User = require("./userModel.js");
+const Boardgame = require("./boardgameModel.js");
+
+// Association User -> Boardgame
+User.belongsToMany(Boardgame, {
+    as: "boardgame",
+    through: "user_boardgame",
+    foreignKey: "user_id",
+    otherKey: "boardgame_id",
+    timestamps: false
+});
+
+Boardgame.belongsToMany(User, {
+    as: "user",
+    through: "user_boardgame",
+    foreignKey: "boardgame_id",
+    otherKey: "user_id",
+    timestamps: false
+});
