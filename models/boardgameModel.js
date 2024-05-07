0249a153a864c3265f680eabf506aab4afcd468d
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-05-07 13:42:54 +0200

    added boardgame model

---
+++ models/boardgameModel.js
@@ -0,0 +1,54 @@
+const sequelize = require("sequelize");
+const db = require("../db/db.js");
+const { DataTypes } = sequelize;
+
+const Boardgame = db.define(
+    "boardgame",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        player_min: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        player_max: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        time: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        type: {
+            type: DataTypes.ENUM("Dés", "Cartes", "Plateau"),
+            allowNull: false,
+        },
+        created_at: {
+            field: "created_at",
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            field: "updated_at",
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    },
+    {
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
+
+module.exports = Boardgame;
