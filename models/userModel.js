a7358dae706a64058a5ca5ddcf7fe837d9dfa185
Morgane-Rabiller <90465154+Morgane-Rabiller@users.noreply.github.com>
2024-05-07 13:30:02 +0200

    added user model

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+const sequelize = require("sequelize");
+const db = require("../db/db.js");
+const { DataTypes } = sequelize;
+
+const User = db.define(
+    "user",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        pseudo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        created_at: {
+            field: "created_at",
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updated_at: {
+            field: "updated_at",
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    },
+    {
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
+
+module.exports = User;
